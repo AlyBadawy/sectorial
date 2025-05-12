e2a8a41cab5c9be6785fe1adee48b492251c5f19
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-12 09:57:03 -0400

    [CHORE TEST] Generators for test files (#4)
    * [CHORE TEST] Generators for test files
    
    * fix rubocop

--- lib/securial/engine.rb
+++ lib/securial/engine.rb
@@ -2,5 +2,18 @@ module Securial
   class Engine < ::Rails::Engine
     isolate_namespace Securial
     config.generators.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: true,
+                       controller_specs: true,
+                       request_specs: true,
+                       model_specs: true
+
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
