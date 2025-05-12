fd7c373310e6c2ee0a37a3bdb32f0285f7fa4676
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-12 09:55:07 -0400

    fix rubocop

--- lib/securial/engine.rb
+++ lib/securial/engine.rb
@@ -5,13 +5,13 @@ module Securial
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: true,
-        controller_specs: true,
-        request_specs: true,
-        model_specs: true
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: true,
+                       controller_specs: true,
+                       request_specs: true,
+                       model_specs: true
 
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
