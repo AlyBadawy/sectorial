fe733b06e71c1c16093568343ba2eebd5a41a14c
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-13 06:48:31 -0400

    [CHORE] Scaffold generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Securial::Engine.routes.draw do
   defaults format: :json do
     get "/status", to: "status#show", as: :status
+
+    namespace Securial.admin_namespace do
+    end
   end
 end
--- lib/securial.rb
+++ lib/securial.rb
@@ -8,6 +8,10 @@ module Securial
   class << self
     attr_writer :configuration
 
+    def admin_namespace
+      configuration.admin_role.to_s.pluralize
+    end
+
     def configuration
       @configuration ||= Configuration.new
     end
