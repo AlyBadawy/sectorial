7cb8262b3d47917ddea5bc14f369bce5bae66e1b
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-15 13:22:03 -0400

    [CHORE] Use UUIDv7 for IDs (#18)

--- db/migrate/20250515104930_create_securial_roles.rb
+++ db/migrate/20250515104930_create_securial_roles.rb
@@ -1,6 +1,6 @@
 class CreateSecurialRoles < ActiveRecord::Migration[8.0]
   def change
-    create_table :securial_roles do |t|
+    create_table :securial_roles, id: :string do |t|
       t.string :role_name
       t.boolean :hide_from_profile, default: false, null: false
 
