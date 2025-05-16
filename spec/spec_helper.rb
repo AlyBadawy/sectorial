90325dc6a3ec896fee8faa27e62190ea677e11c4
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-15 20:59:20 -0400

    retry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "simplecov"
 require "simplecov-lcov"
 
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config.output_directory = "coverage"
+SimpleCov::Formatter::LcovFormatter.config.lcov_file_name = "lcov.info"
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   SimpleCov::Formatter::LcovFormatter
