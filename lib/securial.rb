026f6148ade672479d13033bca296c8e12e09f95
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-18 12:41:50 -0400

    [TEST] increase test coverage (#24)
    * [TEST] increase test coverage
    
    * Fix test scopes
    
    * fix

--- lib/securial.rb
+++ lib/securial.rb
@@ -14,12 +14,24 @@ module Securial
 
     def configure
       yield(configuration)
+      validate_admin_role!
     end
 
     # Returns the pluralized form of the admin role.
     # This behavior is intentional and aligns with the project's routing conventions.
     def admin_namespace
-      configuration.admin_role.to_s.pluralize
+      configuration.admin_role.to_s.pluralize.downcase
+    end
+
+    private
+
+    def validate_admin_role!
+      error_message = "The admin role cannot be 'account' or 'accounts' as it conflicts with the default routes."
+
+      if configuration.admin_role.to_s.pluralize.downcase == "accounts"
+        Securial::ENGINE_LOGGER.error(error_message)
+        raise RuntimeError, error_message
+      end
     end
   end
 end
