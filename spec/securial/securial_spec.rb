0a0cc7089acb7c828900315a013ed1106adf9348
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-18 12:16:56 -0400

    [TEST] increase test coverage

--- spec/securial/securial_spec.rb
+++ spec/securial/securial_spec.rb
@@ -56,4 +56,44 @@ RSpec.describe Securial do
       expect(described_class.configuration).to be_a(Securial::Configuration)
     end
   end
+
+  describe ".validate_admin_role!" do
+    let(:error_message) { "The admin role cannot be 'admin' or 'admins' as it conflicts with the default routes." }
+
+    before do
+      # Reset configuration before each test
+      described_class.configuration = Securial::Configuration.new
+    end
+
+    context "when admin_role conflicts with accounts namespace" do
+      it 'raises error when admin_role is "account"' do
+        described_class.configuration.admin_role = "account"
+
+        expect {
+          described_class.send(:validate_admin_role!)
+        }.to raise_error(RuntimeError, error_message)
+      end
+
+      it 'raises error when admin_role is "accounts"' do
+        described_class.configuration.admin_role = "accounts"
+
+        expect {
+          described_class.send(:validate_admin_role!)
+        }.to raise_error(RuntimeError, error_message)
+      end
+    end
+
+    context "when admin_role is valid" do
+      it "does not raise error for valid role names" do
+        valid_roles = ["manager", "supervisor", "moderator"]
+
+        valid_roles.each do |role|
+          described_class.configuration.admin_role = role
+          expect {
+            described_class.send(:validate_admin_role!)
+          }.not_to raise_error
+        end
+      end
+    end
+  end
 end
