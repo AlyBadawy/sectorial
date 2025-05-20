c7bbccfb51c87a8fdae9f6c027e01c1bd2dad663
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-05-20 08:57:54 -0400

    [FEAT] Add custom error types for configuration and session handling (#29)
    * [FEAT] Refactor logging configuration and session management
    
    Signed-off-by: Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
    
    * [FEAT] Add custom error types for configuration and session handling
    
    Signed-off-by: Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
    
    * Fix JwtHelper
    
    ---------
    
    Signed-off-by: Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>

--- app/helpers/securial/jwt_helper.rb
+++ app/helpers/securial/jwt_helper.rb
@@ -1,45 +1,46 @@
 module Securial
   module JwtHelper
-    def self.encode(session)
-      return nil unless session && session.class == Securial::Session
+    class << self
+      def encode(session)
+        return nil unless session && session.class == Securial::Session
 
-      base_payload = {
-        jti: session.id,
-        exp: expiry_duration.from_now.to_i,
-        sub: "session-access-token",
-        refresh_count: session.refresh_count,
+        base_payload = {
+          jti: session.id,
+          exp: expiry_duration.from_now.to_i,
+          sub: "session-access-token",
+          refresh_count: session.refresh_count,
+        }
 
-      }
+        session_payload = {
+          ip: session.ip_address,
+          agent: session.user_agent,
+        }
 
-      session_payload = {
-        ip: session.ip_address,
-        agent: session.user_agent,
-      }
+        payload = base_payload.merge(session_payload)
+        JWT.encode(payload, secret, algorithm, { kid: "hmac" })
+      end
 
-      payload = base_payload.merge(session_payload)
-      JWT.encode(payload, secret, algorithm, { kid: "hmac" })
-    end
+      def decode(token)
+        decoded = JWT.decode(token, secret, true, { algorithm: algorithm, verify_jti: true, iss: "securial" })
+        decoded.first
+      end
 
-    def self.decode(token)
-      decoded = JWT.decode(token, secret, true, { algorithm: algorithm, verify_jti: true, iss: "securial" })
-      decoded.first
-    end
+      private
 
-    private
+      def secret
+        Securial.validate_session_secret!
+        Securial.configuration.session_secret
+      end
 
-    def self.secret
-      Securial.validate_session_secret!
-      Securial.configuration.session_secret
-    end
+      def algorithm
+        Securial.validate_session_algorithm!
+        Securial.configuration.session_algorithm.to_s.upcase
+      end
 
-    def self.algorithm
-      Securial.validate_session_algorithm!
-      Securial.configuration.session_algorithm.to_s.upcase
-    end
-
-    def self.expiry_duration
-      Securial.validate_session_expiry_duration!
-      Securial.configuration.session_expiration_duration
+      def expiry_duration
+        Securial.validate_session_expiry_duration!
+        Securial.configuration.session_expiration_duration
+      end
     end
   end
 end
